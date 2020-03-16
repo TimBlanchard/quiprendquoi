c44fa7886b6828cdb9a50e5e7f82b12cff75eb0a
TimBlanchard <timothee.blanchard37@gmail.com>
2020-03-16 22:38:54 +0100

    Post vers l'API

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const dotenv = require('dotenv').config()
+const dotenv = require('dotenv').config();
 const bodyParser = require('body-parser');
+const axios = require('axios');
 const app = express();
 
 app.set('view engine', 'pug');
@@ -12,8 +13,10 @@ app.get('/', function(req, res) {
 });
 
 app.post('/party', function(req, res) {
-    console.log(req.body);
-    res.send('Bien rempli !');
+    axios
+        .post(`${process.env.API_URL}/party`, req.body)
+        .then(({data}) => console.log(data))
+        .catch((err) => console.error(err));
 });
 
 app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
