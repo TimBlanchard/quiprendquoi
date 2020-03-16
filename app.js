c853e25de84bc5e61a20b8bc86096dff23b10874
TimBlanchard <timothee.blanchard37@gmail.com>
2020-03-16 21:37:18 +0100

    template pug

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const app = express();
 
+app.set('view engine', 'pug');
+
 
 app.get('/', function(req, res) {
-    res.send('Changement msg tim');
+    res.render('index');
 });
 
 app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
